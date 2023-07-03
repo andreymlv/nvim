ad5f8a4151155fa63679e00d1385041e6ebe4b10
andreymlv <ndrmlv@ya.ru>
2023-07-03 23:15:00 +0300

    Add git support and treesitter

--- lua/andreymlv/plugins.lua
+++ lua/andreymlv/plugins.lua
@@ -3,5 +3,7 @@ require("lazy").setup({
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 }
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "tpope/vim-fugitive" },
 })
