13cac00aed0793386b752aa48a185b181b619fa3
andreymlv <ndrmlv@ya.ru>
2023-07-16 00:01:35 +0300

    format and add comment plugin

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,80 +1,80 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "LSP actions",
-	callback = function(event)
-		local opts = { buffer = event.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set({ "n", "v" }, "<space>la", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "<space>lr", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "<space>lf", function()
-			vim.lsp.buf.format({ async = true })
-		end, opts)
-	end,
+  desc = "LSP actions",
+  callback = function(event)
+    local opts = { buffer = event.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set({ "n", "v" }, "<space>la", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<space>lr", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<space>lf", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+  end,
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup()
 
 local has_words_before = function()
-	unpack = unpack or table.unpack
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local luasnip = require("luasnip")
 local cmp = require("cmp")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-			-- they way you will only jump inside the snippet region
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	}, {
-		{ name = "buffer" },
-	}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  }),
 })
 
 local lspconfig = require("lspconfig")
@@ -82,47 +82,48 @@ local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 lspconfig.clangd.setup({
-	capabilities = lsp_capabilities,
-	cmd = {
-		"clangd",
-		"--offset-encoding=utf-16",
-	},
+  capabilities = lsp_capabilities,
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
 })
 lspconfig.lua_ls.setup({})
 lspconfig.nil_ls.setup({})
 lspconfig.texlab.setup({})
 lspconfig.marksman.setup({})
 lspconfig.qmlls.setup({
-	cmd = { "qmlls6" },
-	filetypes = { "qmljs", "qml" },
+  cmd = { "qmlls6" },
+  filetypes = { "qmljs", "qml" },
 })
+lspconfig.zls.setup({})
 
 require("mason-lspconfig").setup_handlers({
-	function(server_name)
-		lspconfig[server_name].setup({
-			capabilities = lsp_capabilities,
-		})
-	end,
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = lsp_capabilities,
+    })
+  end,
 })
 
 local null_ls = require("null-ls")
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.clang_format,
-		null_ls.builtins.formatting.latexindent,
-		null_ls.builtins.formatting.latexindent,
-		null_ls.builtins.formatting.nixpkgs_fmt,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.qmlformat,
-		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.formatting.stylua,
+  sources = {
+    null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.formatting.latexindent,
+    null_ls.builtins.formatting.latexindent,
+    null_ls.builtins.formatting.nixpkgs_fmt,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.qmlformat,
+    null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.formatting.stylua,
 
-		null_ls.builtins.diagnostics.chktex,
-		null_ls.builtins.diagnostics.qmllint,
-		null_ls.builtins.diagnostics.typos,
+    null_ls.builtins.diagnostics.chktex,
+    null_ls.builtins.diagnostics.qmllint,
+    null_ls.builtins.diagnostics.typos,
 
-		null_ls.builtins.hover.dictionary,
-		null_ls.builtins.hover.printenv,
-	},
+    null_ls.builtins.hover.dictionary,
+    null_ls.builtins.hover.printenv,
+  },
 })
--- lua/andreymlv/plugins.lua
+++ lua/andreymlv/plugins.lua
@@ -1,51 +1,48 @@
 require("lazy").setup({
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"jose-elias-alvarez/null-ls.nvim",
-		build = ":MasonUpdate",
-	},
-	{
-		"kkoomen/vim-doge",
-		build = ":call doge#install()",
-	},
-	{ "lewis6991/gitsigns.nvim" },
-	{ "nvim-lualine/lualine.nvim" },
-	{
-		"peterhoeg/vim-qml",
-		event = "BufRead",
-		ft = { "qml" },
-	},
-
-	{
-		"peterhoeg/vim-qml",
-		event = "BufRead",
-		ft = { "qml" },
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  { "ellisonleao/gruvbox.nvim",        priority = 1000 },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "jose-elias-alvarez/null-ls.nvim",
+    build = ":MasonUpdate",
+  },
+  {
+    "kkoomen/vim-doge",
+    build = ":call doge#install()",
+  },
+  { "lewis6991/gitsigns.nvim" },
+  { "nvim-lualine/lualine.nvim" },
+  {
+    "peterhoeg/vim-qml",
+    event = "BufRead",
+    ft = { "qml" },
+  },
+  {
+    "peterhoeg/vim-qml",
+    event = "BufRead",
+    ft = { "qml" },
+  },
+  { "numToStr/Comment.nvim" },
 })
