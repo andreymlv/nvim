09bf3f3c3e85bfc1ea6ae199e80bc6317f96068a
andreymlv <ndrmlv@ya.ru>
2023-07-15 21:55:20 +0300

    add spell check and sort entries

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -109,17 +109,20 @@ local null_ls = require("null-ls")
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.clang_format,
+		null_ls.builtins.formatting.latexindent,
+		null_ls.builtins.formatting.latexindent,
 		null_ls.builtins.formatting.nixpkgs_fmt,
+		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.qmlformat,
 		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.formatting.latexindent,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.latexindent,
+		null_ls.builtins.formatting.stylua,
 
-		null_ls.builtins.diagnostics.qmllint,
 		null_ls.builtins.diagnostics.chktex,
-		-- null_ls.builtins.completion.spell,
+		null_ls.builtins.diagnostics.qmllint,
+		null_ls.builtins.diagnostics.typos,
+
+		null_ls.builtins.hover.dictionary,
+		null_ls.builtins.hover.printenv,
 	},
 })
